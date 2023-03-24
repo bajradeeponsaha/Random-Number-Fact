7a17d480fcf8d53f95137d172e30808e9969e38f
bajradeeponsaha <bajradeeponsaha5@gmail.com>
2023-03-24 22:20:32 +0530

    Second Commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const btn = document.querySelector("button")
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'd1a0cb3794msh3ad6a229e2e71dep1350bbjsnf0e49a035f55',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
 	}
 };
@@ -15,7 +15,7 @@ function randomFact(){
     fetch('https://numbersapi.p.rapidapi.com/random/trivia?fragment=true&json=true', options)
 	.then(response => response.json())
 	.then(response => {
-        console.log(response)
+        // console.log(response)
         num.textContent = response.number
         fact.textContent = response.text
     })
